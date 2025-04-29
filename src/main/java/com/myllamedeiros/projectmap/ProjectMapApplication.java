f33d50fdba81b67d1144da59d752999589718867
Myllena Medeiros <myllenamed19@gmail.com>
2025-04-28 21:07:34 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/main/java/com/myllamedeiros/projectmap/ProjectMapApplication.java
@@ -0,0 +1,13 @@
+package com.myllamedeiros.projectmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myllamedeiros/projectmap/ProjectMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myllamedeiros.projectmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
