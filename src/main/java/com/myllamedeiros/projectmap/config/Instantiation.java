066a18ec2fb7269d8e2e300ddf5e17dcfcc496b3
AndreVinnis <andrev.macambira@gmail.com>
2025-05-01 11:30:49 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/myllamedeiros/projectmap/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.myllamedeiros.projectmap.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.myllamedeiros.projectmap.domain.User;
+import com.myllamedeiros.projectmap.enums.Campus;
+import com.myllamedeiros.projectmap.enums.Curso;
+import com.myllamedeiros.projectmap.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+		User myllena = new User("498406791", "Myllena de Sousa Medeiros", "mylla@gmail.com", Campus.CAMPUSVII, Curso.COMPUTACAO, sdf.parse("12/05/2005"), 0);
+		
+		userRepository.saveAll(Arrays.asList(myllena));
+	}
+
+}
